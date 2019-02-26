ebdda8e249661c55f940a27da467d5f9e3bcb2ab
Nick-Ballenger <Nick-Ballenger>
2019-02-25 18:11:18 -0600

    MVP in the knick of time

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -99,9 +99,11 @@ for (let i = 0; i < inventory.length; i++) {
 let carYear =[];
 for (let i = 0; i < inventory.length; i++){
     //  carYear.push(inventory[i].car_year)
+    console.log(inventory[i].car_year);
      carYear.push(inventory[i].caryear);
      
 }
+
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
@@ -119,6 +121,13 @@ for (let i = 0; i < inventory.length; i++){
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = [];
-console.log();
+for (let i = 0; i < inventory.length; i++){
+    if(inventory[i].car_make =="BMW"
+    || inventory[i].car_make=="audi"){
+        BMWAndAudi.push(inventory[i]);
+    }
+
+}
+console.log(BMWAndAudi);
 
 
